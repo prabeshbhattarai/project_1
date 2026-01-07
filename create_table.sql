dcd0672b2d3c0525abbdab3e16c656d2c1a68f0a
Prabesh Bhattarai <prabeshbhattarai20@gmail.com>
2026-01-07 15:38:49 -0500

    import all data into tables

--- create_table.sql
+++ create_table.sql
@@ -14,3 +14,26 @@ CREATE TABLE pizza_sales_raw (
   pizza_ingredients TEXT,
   pizza_name        TEXT
 );
+
+
+DROP TABLE IF EXISTS pizza_sales;
+
+CREATE TABLE pizza_sales AS
+SELECT
+  pizza_id,
+  order_id,
+  pizza_name_id,
+  quantity,
+  TO_DATE(order_date, 'DD-MM-YYYY') AS order_date,
+  order_time::time                  AS order_time,
+  unit_price,
+  total_price,
+  pizza_size,
+  pizza_category,
+  pizza_ingredients,
+  pizza_name
+FROM pizza_sales_raw;
+
+
+
+
