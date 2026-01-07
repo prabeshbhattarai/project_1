9390da0d095d1240c7da8c8c021b2ea27d632cec
Prabesh Bhattarai <prabeshbhattarai20@gmail.com>
2026-01-07 16:55:36 -0500

    Percentage of sales by pizza category

---
+++ pizza_project/percsales_pizzacate.sql
@@ -0,0 +1,10 @@
+SELECT
+    pizza_category,
+    ROUND(
+        SUM(total_price) * 100.0 /
+        (SELECT SUM(total_price) FROM pizza_sales)
+    ) || '%' AS per_sales
+FROM
+    pizza_sales
+GROUP BY
+    pizza_category
